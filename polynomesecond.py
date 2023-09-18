ae964c8710204a670d2c580fb4bb5365fa4dda6c
NoDelayToGetDiv <145397786+NoDelayToGetDiv@users.noreply.github.com>
2023-09-18 23:26:27 +0200

    Update polynomesecond.py

--- polynomesecond.py
+++ polynomesecond.py
@@ -1,22 +1,6 @@
 from pyg import*
 from math import*
 def Polynome_du_second_degre(a,b,c):
-     #Aidé par M.Carro notamment sur l'écriture de la variable fact.
-    dis=b**2-4*a*c
-    if (dis>0):
-        r1=(-b-sqrt(dis))/(2*a)
-        r2=(-b+sqrt(dis))/(2*a)
-        fact = str(a) + "(x-" + str(r1) + ")(x-" + str(r2) + ")"
-        return(fact)
-    elif (det==0):
-        r0=-b-sqrt(det)/(2*a)
-        fact = str(a) + "(x-" + str(r0) + ")**2"
-        return(fact)
-    else:
-        return('Pas de racine')
-
-
-def Polynome_du_second_degre2(a,b,c):
     #Aidé par M.Carro notamment sur l'écriture de la variable fact. Cette fois-ci la version est encore plus lisible.
 
     dis = b**2-4*a*c #Discriminant
